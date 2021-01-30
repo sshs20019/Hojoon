b2d2a46e6e48f7a03d9b6b170348162e501c3ac4
sshs20019 <69911753+sshs20019@users.noreply.github.com>
2021-01-31 00:56:41 +0900

    Update and rename hojoon.js to minjae.js

---
+++ minjae.js
@@ -0,0 +1,6 @@
+let alertNum;
+for (alertNum = 100; alertNum > 0; alertNum -= 1) {
+  alert(`경고가 ${alertNum - 1}회 남았어!
+    만약 너가 이걸 모두 직접 눌러서 없앤다면 나는 너를 인정할게!`);
+}
+alert('역시 너는 ㅇㅁ의 전남친답게 끈기가 대단하구나! 너는 서울대 컴공에 들어갈 수 있을거야!!');
