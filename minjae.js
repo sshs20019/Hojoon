a740d7560f4bfd3c23de1ff60ff6853dd815a27b
sshs20019 <69911753+sshs20019@users.noreply.github.com>
2021-01-31 01:46:48 +0900

    Update minjae.js

--- minjae.js
+++ minjae.js
@@ -1,10 +1,10 @@
 let alertNum;
 for (alertNum = 100; alertNum > 51; alertNum -= 1) {
-  alert(`경고가 ${alertNum - 1}회 남았어!
-         만약 너가 이걸 모두 직접 눌러서 없앤다면 나는 너를 인정할게!`);
+  alert(`경고가 ${alertNum - 1}회 남았어!\n만약 너가 이걸 모두 직접 눌러서 없앤다면 나는 너를 인정할게!`);
+    play();
 }
-for (alertNum = 100; alertNum > 1; alertNum -= 1) {
-  alert(`경고가 ${alertNum - 1}회 남았어!
-         이렇게 끈질길 줄은 몰랐는걸? 역시 조민제는 보통 조민제가 이니네!`);
+for (; alertNum > 1; alertNum -= 1) {
+  alert(`경고가 ${alertNum - 1}회 남았어!\n이렇게 끈질길 줄은 몰랐는걸? 역시 조민제는 보통 조민제가 이니네!`);
+    play();
 }
 alert('역시 너는 ㅇㅁ의 전남친답게 끈기가 대단하구나! 너는 서울대 컴공에 들어갈 수 있을거야!!');
